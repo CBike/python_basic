c70625cbe29f9c1c2525bbd00db2a20a8ddaeaf2
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-01-04 23:18:08 +0900

    study

---
+++ practice.py
@@ -0,0 +1,22 @@
+# 자료형
+#숫자형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*3)
+print(3*(3+1))
+#문자형
+print('풍선')
+print('나비')
+print('ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+print('ㅋ'*9)
+#boolean
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
