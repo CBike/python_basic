9a0bb8e779f000965bd84c00656020f0f773cd58
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-01-06 02:47:40 +0900

    study

--- Quiz).py
+++ Quiz).py
@@ -7,11 +7,12 @@ Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하
 규칙3) : 남은 글자 중 처음 세자리  + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
 """
 
-url = 'http://naver.com'
+# url = 'http://naver.com'
+url = 'http://google.com'
 
 step1 = url.replace('http://','')
 step2 = step1[:step1.index('.')]
-step3 = f'{step2[:3]}{len(step2)}{step2.count("e")}!'
+password = f'{step2[:3]}{len(step2)}{step2.count("e")}!'
 print(step1)
 print(step2)
-print(step3)
+print(password)
