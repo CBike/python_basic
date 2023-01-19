a4388bd8a564e2e4e0e3858289e5b8f8e29e3a44
yangho.jeon <64768242+CBike@users.noreply.github.com>
2023-01-19 16:19:04 +0900

    study

---
+++ text_starcraft.py
@@ -0,0 +1,144 @@
+from random import *
+
+class Unit:
+    def __init__(self, name, hp, speed):
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+        print(f"{self.name}이 생성 되었습니다.")
+
+    def move(self, location):
+        print(f"지상유닛 이동")
+        print(f"{self.name} : {location} 방향으로 이동 합니다 . 속도 : {self.speed}")
+
+    def damaged(self,damage):
+        print(f"{self.name}:{damage} 데미지를 입었습니다.")
+        self.hp -= damage
+        print(f"{self.name}:현재 체력은 {self.hp} 입니다.")
+        if self.hp <= 0:
+            print(f"{self.name}: 파괴 되었습니다.")
+
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
+
+    def attack(self, location):
+        print(f"{self.name}:{location}방향 으로 적군을 공격 합니다.[공격력{self.damage}]")
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print(f"{name};{location}방향으로 날아갑니다.속도: {self.flying_speed}")
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage)
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동] ")
+        self.fly(self.name, location)
+
+class Marine(AttackUnit):
+    # 스팀팩
+    def __init__(self):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print(f"{self.name} : 스팀팩을 사용 합니다 HP 10 감소 ")
+
+        else:
+            print(f"{self.name}: 체력이 부족하여 스팀팩을 사용하지 않습니다.")
+
+
+
+class Tank(AttackUnit):
+    #시즈 모드
+    seize_developed = False
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+        self.seize_mode = False
+
+    def set_seize_mode(self):
+
+        if Tank.seize_developed == False:
+            return
+
+        if not self.seize_mode:
+            print(f"{self.name}: 시즈 모드로 전환 합니다.")
+            self.damage *= 2
+            self.seize_mode = True
+        else:
+            print(f"{self.name}: 시즈 모드를 해제 합니다.")
+            self.damage /= 2
+            self.seize_mode = False
+
+
+class wraith(FlyableAttackUnit):
+        def __init__(self):
+            super().__init__("레이스", 80, 20, 5)
+            self.clocked = False # 클로킹 모드
+
+        def clocking(self):
+            if self.clocked:
+                print(f"{self.name} :  클로킹 모드 해제 합니다.")
+                self.clocked = False
+            else:
+                print(f"{self.name} : 클로킹 모드 설정 합니다.")
+                self.clocked = True
+
+def game_start():
+    print("[ 알림 ] 새로운 게임을 시작 합니다.")
+
+def game_over():
+    print("Player : GG !!!")
+    print("[Player] 님이 퇴장 하였습니다.")
+
+
+game_start()
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+T1 = Tank()
+T2 = Tank()
+
+w1 = wraith()
+
+attack_units = []
+
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(T1)
+attack_units.append(T2)
+attack_units.append(w1)
+
+for unit in attack_units:
+    unit.move("1시")
+
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료")
+
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, wraith):
+        unit.clocking()
+
+for unit in attack_units:
+    unit.attack("1시")
+
+for unit in attack_units:
+    unit.damaged(randint(5, 21))
+
+game_over()
+
+
